89584002d72f790c2577681f5683187b0bcd1152
unratedpupet <zphillips.de@gmail.com>
2023-05-11 22:32:22 -0400

    Added functionality to increase the click amount

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 // Get references to the button and counter elements
-const button = document.getElementById('clickButton');
+const clickButton = document.getElementById('clickButton');
 const counter = document.getElementById('counter');
+const incrementIncreaseButton = document.getElementById('incrementIncrease');
+
+// Initialize the increment value
+let incrementValue = 1;
 
 // Initialize the counter value
 let count = 0;
 
 // Function to increment the counter and update the display
 function incrementCounter() {
-  count++;
+  count += incrementValue;
   counter.textContent = count;
 }
 
-// Add a click event listener to the button
-button.addEventListener('click', incrementCounter);
+// Add a click event listener to the clickMe button
+clickButton.addEventListener('click', incrementCounter);
+
+// Function to increase the increment amount
+function incrementIncrease() {
+    if (count >= 10) {
+        incrementValue *= 2;
+        count -= 10;
+        counter.textContent = count;
+    }
+}
+
+// Add a click event listener to the increaseIncrement button
+incrementIncreaseButton.addEventListener('click', incrementIncrease);
